44158c55af7bd5d4948f86608687f360a98514c0
flow6852 <flow6852math@gmail.com>
2023-04-13 01:27:53 +0900

    init

---
+++ denops/@ddu-kinds/vim_type.ts
@@ -0,0 +1,102 @@
+import * as fn from "https://deno.land/x/denops_std@v4.1.3/function/mod.ts";
+import type {
+  Actions,
+  Item,
+} from "https://deno.land/x/ddu_vim@v2.7.0/types.ts";
+import {
+  ActionFlags,
+  BaseKind,
+} from "https://deno.land/x/ddu_vim@v2.7.0/types.ts";
+
+export interface ActionData {
+  value: string;
+  type: string;
+  scope?: string;
+}
+
+type PreviewParams = {
+  kind?: string;
+}
+
+export class Kind extends BaseKind<Params> {
+  override actions: Actions<Params> = {
+    yank: async (args: { denops: Denops; items: DduItem[] }) => {
+      for (const item of args.items) {
+        const action = item?.action as ActionData;
+        const value = action.value;
+await fn.setreg(args.denops, '"', value, "v");
+        await fn.setreg(
+          args.denops,
+          await vars.v.get(args.denops, "register"),
+          value,
+          "v",
+        );
+      }
+
+      return ActionFlags.Persist;
+    },
+  };
+
+  override async getPreviewer(
+    args: {
+      denops: Denops;
+      item: DduItem;
+      actionParams: unknown;
+      previewContext: PreviewContext;
+    },
+  ): Promise<Previewer | undefined> {
+    const params = args.actionParams as PreviewParams;
+    const action = args.item.action as ActionData;
+    if (!action) {
+      return await Promise.resolve(undefined);
+    }
+    let ret: Previewer;
+    switch (params.kind) {
+      case "help":
+        ret =  await getFromHelp(args.denops, args.item.word)
+        break;
+      case "value":
+        ret = {
+          kind: "nofile",
+          contents: showValue(args.item),
+        };
+        break;
+      default:
+        ret = {
+          kind: "nofile",
+          contents: showValue(args.item),
+        };
+        break;
+    }
+    return await Promise.resolve(ret);
+  };
+
+  override params(): Params {
+    return {
+    };
+  }
+}
+
+async function getFromHelp(denops: Denops, word: string): Array<string>{
+  let ret = {kind: "nofile", context: ["don't exist help file"]}
+  const files = (await fn.globpath(denops, await fn.getbufvar(denops, await fn.bufnr(denops, "%"), "&rtp") as Array<string>, "doc/*.txt") as string).split("\n")
+  for (const file of files){
+    const lines = (await Deno.readTextFile(file)).split("\n")
+    for(let i = 0; i < lines.length; i++){
+      if(lines[i].indexOf("\*" + word + "\*") > -1){
+        ret = {
+          kind: "buffer",
+          path: file,
+          lineNr: i
+        }
+        break
+      }
+    }
+  }
+  return ret
+}
+
+// need parse?
+function showValue(item: Item<ActionData>[]): Array<string>{
+  return [item.action.value] ?? ["undefined"]
+}
